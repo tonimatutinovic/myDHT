803a4973bd90f4194a5553cd1cd438de1d08e243
Toni Matutinović <tonimatutta@gmail.com>
2025-12-01 20:01:38 +0100

    Update BasicRead example: add DHT22 support with comments

--- examples/BasicRead/BasicRead.ino
+++ examples/BasicRead/BasicRead.ino
@@ -1,25 +1,34 @@
-#include<myDHTlib.h>
+#include <myDHTlib.h>
 
 const int DHT_PIN = 2;
 
-MyDHT dht(DHT_PIN);
+// For DHT11
+MyDHT dht(DHT_PIN, DHT11);
 
-void setup(){
+// For DHT22, uncomment the line below and comment out the DHT11 line
+// MyDHT dht(DHT_PIN, DHT22)
+
+void setup()
+{
   Serial.begin(115200);
   dht.begin();
 
   delay(2000);
 }
 
-void loop(){
-  if(dht.read()) {
+void loop()
+{
+  if (dht.read())
+  {
     Serial.print("Temperature: ");
     Serial.println(dht.getTemperature());
     Serial.print("Humidity: ");
     Serial.println(dht.getHumidity());
     Serial.println("----------------");
-  } else {
-    Serial.println("Greška u čitanju!");
+  }
+  else
+  {
+    Serial.println("Failed to read from sensor!");
   }
   delay(2000);
 }
